54356240995f1c556a930cf6332d662d3f340608
jukkanghost <jukkanghost@gmail.com>
2024-02-15 12:58:53 +0100

    deleteAll delete update create

--- src/main/java/com/microcompany/accountsservice/controller/AccountsController.java
+++ src/main/java/com/microcompany/accountsservice/controller/AccountsController.java
@@ -1,27 +1,56 @@
 package com.microcompany.accountsservice.controller;
 
+import com.microcompany.accountsservice.exception.AccountNotfoundException;
 import com.microcompany.accountsservice.model.Account;
 import com.microcompany.accountsservice.persistence.AccountRepository;
+import com.microcompany.accountsservice.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Min;
 
 @RestController
 @RequestMapping("/accounts")
+@Validated
 public class AccountsController {
 
     @Autowired
-    AccountRepository repo;
+    AccountService service;
 
     @GetMapping(value = "")
-    public ResponseEntity<Object> getAll(){
-        return ResponseEntity.status(HttpStatus.OK).body(repo.findAll());
+    public ResponseEntity<Object> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getAccounts());
     }
 
     @PostMapping(value = "")
-    public ResponseEntity<Object> create(@RequestBody Account account){
-        return ResponseEntity.status(HttpStatus.CREATED).body(repo.save(account));
+    public ResponseEntity<Object> create(@RequestBody Account account) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(account));
+    }
+
+    @DeleteMapping(value = "/deleteAll/{ownerId}")
+    public ResponseEntity deleteAll(@PathVariable("ownerId") @Min(1) Long ownerId) {
+        service.deleteAccountsUsingOwnerId(ownerId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Object> getOne(@PathVariable("id") @Min(1) Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getAccount(id));
+    }
+
+    @PutMapping(value = "/{id}", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Object> update(@PathVariable("id") @Min(1) Long id, @RequestBody Account account) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.updateAccount(id, account));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity delete(@PathVariable("id") @Min(1) Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/microcompany/accountsservice/model/Account.java
+++ src/main/java/com/microcompany/accountsservice/model/Account.java
@@ -5,6 +5,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 
 import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 
 @Getter
@@ -13,6 +14,7 @@ import java.util.Date;
 @AllArgsConstructor
 @Entity
 @Table(name = "accounts")
+@XmlRootElement
 public class Account {
 
     @Id
