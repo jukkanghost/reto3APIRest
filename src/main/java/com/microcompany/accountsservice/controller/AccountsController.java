7b0584d9ffaeb1e15bd53231247751e252959cf6
jukkanghost <jukkanghost@gmail.com>
2024-02-15 12:30:51 +0100

    create

--- src/main/java/com/microcompany/accountsservice/controller/AccountsController.java
+++ src/main/java/com/microcompany/accountsservice/controller/AccountsController.java
@@ -1,12 +1,11 @@
 package com.microcompany.accountsservice.controller;
 
+import com.microcompany.accountsservice.model.Account;
 import com.microcompany.accountsservice.persistence.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/accounts")
@@ -18,6 +17,11 @@ public class AccountsController {
     @GetMapping(value = "")
     public ResponseEntity<Object> getAll(){
         return ResponseEntity.status(HttpStatus.OK).body(repo.findAll());
+    }
+
+    @PostMapping(value = "")
+    public ResponseEntity<Object> create(@RequestBody Account account){
+        return ResponseEntity.status(HttpStatus.CREATED).body(repo.save(account));
     }
 
 }
