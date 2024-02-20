bce6ecfa84b383e409070791db04540e9b8fabaa
cpanap00 <cpanap00@estudiantes.unileon.es>
2024-02-20 11:50:31 +0100

    Test controller

--- src/test/java/com/microcompany/accountsservice/controller/AccountsserviceControllerTest.java
+++ src/test/java/com/microcompany/accountsservice/controller/AccountsserviceControllerTest.java
@@ -2,8 +2,12 @@ package com.microcompany.accountsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
+@ActiveProfiles({"test"})
+@Sql("classpath:datos_prueba.sql")
 public class AccountsserviceControllerTest {
 
     @Autowired
